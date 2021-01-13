47f7ed14474dc2094fb050c2efd5dd3038308d07
Samantha Ernano <samantha.ernano@gmail.com>
2021-01-13 13:31:35 -0500

    Added bash configs and tty colors script

---
+++ _colors-tty.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+[ "${TERM:-none}" = "linux" ] && \
+    printf '%b' '\e]P00d0d11
+                 \e]P154414C
+                 \e]P29E4E4E
+                 \e]P3B6574E
+                 \e]P4538877
+                 \e]P5EF9F6A
+                 \e]P6E4BB6D
+                 \e]P7f1eeca
+                 \e]P8a8a68d
+                 \e]P954414C
+                 \e]PA9E4E4E
+                 \e]PBB6574E
+                 \e]PC538877
+                 \e]PDEF9F6A
+                 \e]PEE4BB6D
+                 \e]PFf1eeca
+                 \ec'
